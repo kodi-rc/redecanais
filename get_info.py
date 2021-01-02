f2ddcb246ef0f76a23c8229aa7c2a8054ba4fe69
kodi-rc <60229464+kodi-rc@users.noreply.github.com>
2021-01-02 18:52:19 -0300

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -7,10 +7,95 @@ import os
 os.environ["PYTHONIOENCODING"] = "utf-8"
 
 import tmdbsimple as tmdb
-myKey = ''
+myKey = 'db175ebea0f1f462b6a5f90eb5be454a' #add an option for the user to put their TMDB key
 tmdb.API_KEY = myKey
-tmdb_idioma = 'pt-BR'
-#---------------------------------
+TMDB_LANG = 'pt-BR' #User local language
+
+URLYOUTUBE = 'https://www.youtube.com/watch?v='
+URLTMDBPOSTER = 'https://image.tmdb.org/t/p/w'
+URLNOPOSTER = 'https://nocover.png'
+
+def movie_detail(tvg_tmdbid='', postersize='200'):
+    '''
+    tvg_tmdbid = Movie id on TMDB website
+        type = string
+        ex: Os Vingadores : https://www.themoviedb.org/movie/24428-the-avengers?language=pt-BR
+        tvg_tmdbid = '24428'
+
+    postersize = Poster image width
+        type = string
+        ex: 200 , 300, 500 ...
+    '''
+    if tvg_tmdbid != None and str(tvg_tmdbid).strip() != '':
+        tmdb_movie = tmdb.Movies(tvg_tmdbid)
+        tmdb_infos = tmdb_movie.info(language=TMDB_LANG)
+
+        originaltitle = tmdb_infos['original_title']
+        print(f'TMDB Movie ID: {tvg_tmdbid}\n'
+            f'Original Title: {originaltitle}\n')
+
+        poster = ''
+        posterpath = tmdb_infos['poster_path']
+        if posterpath != '' and posterpath != 'null' and posterpath != '0' and posterpath != None:
+            poster = URLTMDBPOSTER + postersize + posterpath
+        else:
+            poster = URLNOPOSTER
+        print(f'Poster: {poster}\n')
+
+        overview = ''
+        overview_mylang  = tmdb_infos['overview']
+        overview_mylang = overview_mylang.strip()
+        if overview_mylang != '' and overview_mylang != 'null' and overview_mylang != '0' and overview_mylang != None:
+            overview = overview_mylang
+        else:
+            overview_en  = tmdb_movie.info()['overview']
+            overview_en = overview_en.strip()
+            if overview_en != '' and overview_en != 'null' and overview_en != '0':
+                overview = f'Only in English - {overview_en}'
+            else:
+                overview = ''
+
+        if overview != '':
+            overview = overview
+            print(f'Overview: {overview}\n')
+        else:
+            print('Overview not available on TMD\n')
+
+
+        key_trailer = ''
+        lang_trailer = TMDB_LANG.upper()
+        trailers_mylang = tmdb_movie.videos(language=TMDB_LANG)['results']
+        if len(trailers_mylang) > 0:
+            key_trailer = trailers_mylang[0]['key']
+            key_trailer = key_trailer.strip()
+        else:
+            trailers_en = tmdb_movie.videos()['results']
+            if len(trailers_en) > 0:
+                key_trailer = trailers_en[0]['key']
+                key_trailer = key_trailer.strip()
+                lang_trailer = 'EN'
+
+        if key_trailer != '':
+            print(f'Trailer: {lang_trailer} | {URLYOUTUBE}{key_trailer}\n')
+        else:
+            print('Trailer not available on TMD\n')
+
+    else:
+        print('TMDB Movie ID not informed\n')
+
+    print('==========================\n')
+
+#poster: ok, overview: pt-BR , trailer: pt-BR
+movie_detail('24428')
+
+#poster: ok, overview: pt-BR , trailer: English
+movie_detail('89')
+
+#poster: ok, overview: English , trailer: English
+movie_detail('438890')
+
+#poster: nothing, overview: English , trailer: nothing
+movie_detail('740437')
 
 
 def serie_detail(tvg_tmdbid='', postersize='200'):
@@ -24,56 +109,51 @@ def serie_detail(tvg_tmdbid='', postersize='200'):
         type = string
         ex: 200 , 300, 500 ...
     '''
-    URLYOUTUBE = 'https://www.youtube.com/watch?v='
-    URLTMDBPOSTER = 'https://image.tmdb.org/t/p/w'
-    URLNOPOSTER = 'https://nocover.png'
 
     if tvg_tmdbid != None and str(tvg_tmdbid).strip() != '':
-        originaltitle = tmdb.TV(tvg_tmdbid).info()['original_name']
+        tmdb_serie = tmdb.TV(tvg_tmdbid)
+        tmdb_infos = tmdb_serie.info(language=TMDB_LANG)
+
+        originaltitle = tmdb_infos['original_name']
         print(f'TMDB Serie ID: {tvg_tmdbid}\n'
             f'Original Title: {originaltitle}\n')
 
-        posters = ''
-        posters = tmdb.TV(tvg_tmdbid).images()['posters']
-        if posters != []:
-            posterpath = posters[0]['file_path']
-            posterpath = posterpath.strip()
-            if posterpath != '' and posterpath != 'null' and posterpath != '0':
-                poster = URLTMDBPOSTER + postersize + posterpath
-            else:
-                poster = URLNOPOSTER
-            print(f'Poster: {poster}\n')
+        poster = ''
+        posterpath = tmdb_infos['poster_path']
+        if posterpath != '' and posterpath != 'null' and posterpath != '0' and posterpath != None:
+            poster = URLTMDBPOSTER + postersize + posterpath
         else:
-            print(f'Poster not available on TMD\n')     
-
+            poster = URLNOPOSTER
+        print(f'Poster: {poster}\n')
 
         overview = ''
-        overview_mylang  = tmdb.TV(tvg_tmdbid).info(language=tmdb_idioma)['overview']
+        overview_mylang  = tmdb_infos['overview']
         overview_mylang = overview_mylang.strip()
-        if overview_mylang != '' and overview_mylang != 'null' and overview_mylang != '0':
+        if overview_mylang != '' and overview_mylang != 'null' and overview_mylang != '0' and overview_mylang != None:
             overview = overview_mylang
         else:
-            overview_en  = tmdb.TV(tvg_tmdbid).info()['overview']
+            overview_en  = tmdb_serie.info()['overview']
             overview_en = overview_en.strip()
-            if overview_en != '' and overview_en != 'null' and overview_en != '0':
+            if overview_en != '' and overview_en != 'null' and overview_en != '0' and overview_en != None:
                 overview = f'Only in English - {overview_en}'
             else:
                 overview = ''
 
         if overview != '':
+            overview = overview
             print(f'Overview: {overview}\n')
         else:
             print('Overview not available on TMD\n')
 
 
         key_trailer = ''
-        lang_trailer = tmdb_idioma.upper()
-        trailers_mylang = tmdb.TV(tvg_tmdbid).videos(language=tmdb_idioma)['results']
+        lang_trailer = TMDB_LANG.upper()
+        trailers_mylang = tmdb_serie.videos(language=TMDB_LANG)['results']
         if len(trailers_mylang) > 0:
             key_trailer = trailers_mylang[0]['key']
             key_trailer = key_trailer.strip()
         else:
-            trailers_en = tmdb.TV(tvg_tmdbid).videos()['results']
+            trailers_en = tmdb_serie.videos()['results']
             if len(trailers_en) > 0:
                 key_trailer = trailers_en[0]['key']
                 key_trailer = key_trailer.strip()
@@ -89,7 +169,7 @@ def serie_detail(tvg_tmdbid='', postersize='200'):
 
     print('==========================\n')
 
-'''
+
 #poster: ok, overview: pt-BR , trailer: pt-BR
 serie_detail('1399')
 
@@ -101,8 +181,6 @@ serie_detail('36', '500')
 
 #poster: nothing, overview: nothing , trailer: nothing
 serie_detail('62608')
-'''
-
 def movie_detail(tvg_tmdbid='', postersize='200'):
     '''
     tvg_tmdbid = Movie id on TMDB website
